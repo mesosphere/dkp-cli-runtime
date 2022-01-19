31aacd3b80136d8f7de56f30b9553569ab4e924d
Florian Reiterer <me@florianreiterer.com>
2022-01-19 12:18:43 +0000

    fix: usage for help flag in subcommands does not start upper-cased

--- runtime_core/cmd/root/root_test.go
+++ runtime_core/cmd/root/root_test.go
@@ -1,6 +1,7 @@
 package root_test
 
 import (
+	"bytes"
 	"io"
 	"testing"
 
@@ -28,6 +29,7 @@ func TestNewCommand(t *testing.T) {
 	assert.ElementsMatch([]string{"verbose", "v"}, flagNames(rootCmd.PersistentFlags(), true))
 
 	assert.NotNil(rootOptions.Profiling)
+	assert.NotNil(rootOptions.Output)
 }
 
 func commandNames(commands []*cobra.Command, onlyVisible bool) []string {
@@ -53,4 +55,39 @@ func flagNames(flags *pflag.FlagSet, onlyVisible bool) []string {
 		}
 	})
 	return names
+}
+
+func TestEnsureTitleCaseForHelpFlagUsage(t *testing.T) {
+	assert := assert.New(t)
+
+	output := bytes.Buffer{}
+	rootCmd, _ := root.NewCommand(&output, &output)
+	rootCmd.SetOut(&output)
+	rootCmd.SetErr(&output)
+
+	subCmd := &cobra.Command{
+		Use: "subcommand",
+		Run: func(cmd *cobra.Command, args []string) {},
+	}
+	rootCmd.AddCommand(subCmd)
+
+	output.Reset()
+	rootCmd.SetArgs([]string{"--help"})
+	rootCmd.Execute()
+	assert.Regexp("-h, --help\\W+Help for root.test\n", output.String())
+
+	output.Reset()
+	rootCmd.SetArgs([]string{"help"})
+	rootCmd.Execute()
+	assert.Regexp("-h, --help\\W+Help for root.test\n", output.String())
+
+	output.Reset()
+	rootCmd.SetArgs([]string{"subcommand", "--help"})
+	rootCmd.Execute()
+	assert.Regexp("-h, --help\\W+Help for subcommand\n", output.String())
+
+	output.Reset()
+	rootCmd.SetArgs([]string{"help", "subcommand"})
+	rootCmd.Execute()
+	assert.Regexp("-h, --help\\W+Help for subcommand\n", output.String())
 }
