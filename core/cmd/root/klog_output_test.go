c3fbca5fed8dff55b2079e4bee809b68d7dd1c98
Martin Hrabovcin <mhrabovcin@68905.no-reply.drupal.org>
2022-01-31 13:20:54 +0100

    fix: remove unnecessary assertions

--- core/cmd/root/klog_output_test.go
+++ core/cmd/root/klog_output_test.go
@@ -56,15 +56,11 @@ func TestKlogOutput(t *testing.T) {
 	klog.V(1).Info("info message")
 	assert.Empty(o.msg)
 	assert.Nil(o.err)
-	// TODO(mh): figure why 1 is expected here
-	// assert.Equal(1, o.verbosity)
 	o.Reset()
 
 	klog.V(2).Info("info message")
 	assert.Empty(o.msg)
 	assert.Nil(o.err)
-	// TODO(mh): figure why 2 is expected here
-	// assert.Equal(2, o.verbosity)
 	o.Reset()
 
 	t.Run("verbosity 1", func(t *testing.T) {
