a4d6bd2c26cd62b7548fc76fb788675e765a5281
Daniel Lipovetsky <dlipovetsky@d2iq.com>
2022-04-06 12:50:23 -0700

    Add license headers to new files

--- core/output/discarding.go
+++ core/output/discarding.go
@@ -1,3 +1,6 @@
+// Copyright 2022 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package output
 
 // NewDiscardingOutput returns an Output that discards all output. Useful for testing, among other purposes.
